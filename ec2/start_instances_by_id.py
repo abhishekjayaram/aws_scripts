edd9c482b232f67ec1a7385a1772623e87efe56e
abhishekjayaram <abhishekjayaramk@gmail.com>
2020-09-14 15:35:20 +0530

    start by instance id

--- ec2/start_instances_by_id.py
+++ ec2/start_instances_by_id.py
@@ -1,10 +1,7 @@
 import boto3
+instance_id = ""
 
 ec2 = boto3.resource('ec2')
-instance = ec2.Instance('i-07d5a54becbf0361a')
+instance = ec2.Instance(instance_id)
 
-# response = instance.start()
-
-for tags in instance.tags:
-    if tags['Key'] == 'Name':
-        print(tags['Value'])
+instance.start()
